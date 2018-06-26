1050d64a47b625df3b32eb3fbf8ab17fb12f67ef
Jasonwang911 <396689144@qq.com>
2018-06-26 23:11:01 +0800

    创建项目

---
+++ src/components/inlay.index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>我是inlay入口的首页</h1>
+      <router-link>
+          <button>跳转到app入口和inlay公用的详情页</button>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'inlayIndex'
+}
+</script>
+
+<style>
+</style>
---
+++ src/entry/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    1111
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+}
+</style>
